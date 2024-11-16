be8fa598abec3d0acc7fa25254a37aaa09f8e0d7
blujai831 <webmaster@blujai831.dev>
2024-11-15 17:29:15 -0800

    Add kinematic Rigidbody2D to all barcodes to ensure the scanner laser is able to collide with them. Colliders remain triggers so as not to impede physics of containing groceries.

--- Assets/Scripts/Barcode.cs
+++ Assets/Scripts/Barcode.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class Barcode : MonoBehaviour
 {
     public bool CorrectlyOriented() {
