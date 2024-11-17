06ba13df637700184e2108c2aa69c318e2857043
cornerloan <connorlowe21@gmail.com>
2024-11-17 15:57:47 -0800

    added high score and play again button

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
     public Image circleImage;
     public float totalTime = 5f;
     [SerializeField] private float timeRemaining;
+    [SerializeField] private Button playAgainButton;
+    [SerializeField] private Button scanButton;
+    [SerializeField] private TMP_Text highScoreText;
+    [SerializeField] private SpawnManager spawnManager;
 
-    void Start()
+    [SerializeField]
+    private void Start()
     {
         timeRemaining = totalTime;
         circleImage.fillAmount = 1f;
+        playAgainButton.onClick.AddListener(PlayAgain);
+        playAgainButton.gameObject.SetActive(false);
+        scanButton.gameObject.SetActive(true);
+        highScoreText.gameObject.SetActive(false);
+        if (PlayerPrefs.GetInt("highscore") == 0) PlayerPrefs.SetInt("highscore", 0);
     }
 
     void Update()
@@ -25,6 +37,24 @@ public class Timer : MonoBehaviour
         else
         {
             circleImage.fillAmount = 0f;
+            GameEnded();
         }
+    }
+
+    void GameEnded()
+    {
+        playAgainButton.gameObject.SetActive(true);
+        scanButton.gameObject.SetActive(false);
+        highScoreText.gameObject.SetActive(true);
+        if (spawnManager.GetScore() > PlayerPrefs.GetInt("highscore"))
+        {
+            PlayerPrefs.SetInt("highscore", spawnManager.GetScore());
+        }
+        highScoreText.text = "High Score: " + PlayerPrefs.GetInt("highscore").ToString();
+    }
+
+    void PlayAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
